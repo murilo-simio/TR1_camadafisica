7ab01774abdb79e78b341e3c88e99b7b3228cd8b
murilo-simio <murilo2110@hotmail.com>
2022-08-28 21:22:41 -0300

    Main e camada_fisica

---
+++ cf_build/CMakeFiles/TR1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TR1.dir/main.cpp.o"
+  "CMakeFiles/TR1.dir/main.cpp.o.d"
+  "TR1"
+  "TR1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TR1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
